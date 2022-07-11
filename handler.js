502dffe1851da041099ebe63888a58a43c641450
Leonardo Fabio Mercado Benítez <lfmercadobe@unal.edu.co>
2022-07-10 20:27:15 -0500

    Se realiza el primer deploy

--- handler.js
+++ handler.js
@@ -5,7 +5,7 @@ module.exports.hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Go Serverless v3.0! Your function executed successfully!",
+        message: "Hola mundo desde mi API",
         input: event,
       },
       null,

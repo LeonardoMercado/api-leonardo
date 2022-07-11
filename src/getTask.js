da5a82ba0438cf91f5c7b7db0ca9c04e9c6f238d
Leonardo Fabio Mercado Benítez <lfmercadobe@unal.edu.co>
2022-07-10 23:43:25 -0500

    Se crea la función para obtener los datos de la tabla

---
+++ src/getTask.js
@@ -0,0 +1,19 @@
+const AWS = require('aws-sdk');
+
+const getTask = async(event) =>{
+    const dynamodb = new AWS.DynamoDB.DocumentClient();
+    const result = await dynamodb.scan({
+        TableName: 'TaskTable',
+    }).promise();
+
+    const tasks = result.Items;
+
+    return {
+        statusCode: 200,
+        body: tasks,
+    };
+};
+
+module.export = {
+    getTask,
+}
